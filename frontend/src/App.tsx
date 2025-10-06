25dac4ec51cf9a915858ffa71489af988e5ecb4a
riku-d <rohitdutta2103@gmail.com>
2025-10-06 23:52:24 +0530

    update react components in App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense, lazy } from 'react'
+import  { Suspense, lazy } from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
 import Header from './components/Header'
 // Import components with lazy loading
